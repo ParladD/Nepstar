0abcb103fb0dd5e7f01411916925a7b8be1ba1b6
Parlad <Pdgna44@gmail.com>
2018-08-24 11:02:09 -0300

    Login view, Resgister view, and email and password validation check done

---
+++ Nepstar/Controllers/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Nepstar
+//
+//  Created by Parlad Dhungana on 2018-08-24.
+//  Copyright © 2018 Parlad Dhungana. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController{
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
